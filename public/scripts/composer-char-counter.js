23f876d4f0e08136b6acce363d9c0e401d9666bc
Alex Cheung <alexc.sfu@gmail.com>
2022-05-25 03:29:44 +0000

    added word counter js file, and added tweets container elements in index.html

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#tweet-text').on('input', function(){
+    const wordLimit = 140;
+    let wordInput = $(this).val();
+    let wordLeft = wordLimit - wordInput.length;
+    $('.counter').html(wordLeft);
+    if(wordLeft < 0) {
+      $('.counter').css('color', 'red');
+    } else {
+      $('.counter').css('color', 'black');
+    }
+  });
+});
