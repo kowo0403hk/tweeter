cb4c16fb62e6f71e1406463a8648913c5b93e25e
Alex Cheung <alexc.sfu@gmail.com>
2022-05-26 06:52:42 +0000

    added dynamic tweets and refetch functionality, also added stretch scrolling funcationality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,40 +1,71 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+////////////////////////////////////////////////////////////////////////
+// document.ready()
+////////////////////////////////////////////////////////////////////////
+
+$(() => {  
+  $('.wordExc').hide();
+  $('.wordNone').hide();
+
+  $('.createNew').on('click', function() {
+    $('.new-tweet').toggle('slow');
+    $('#tweet-text').focus();
+  });
+
+  $('.new-tweet-form').on('submit', (event) => {
+    event.preventDefault();
+    const $form = $('.new-tweet-form');
+    const serializedWord = $form.serialize();
+
+    if (serializedWord.length > 145) {
+      $('.wordExc').slideDown();
+    } else if (serializedWord.length <= 5) {
+      $('.wordNone').slideDown();
+    } else {
+      $('#tweet-text').val('');
+      $('.counter').val('140');
+      $('.wordExc').hide();
+      $('.wordNone').hide();
+      $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8080/tweets',
+        data: serializedWord,
+        success: function () {
+          $('#tweets-container').empty();
+          loadTweets();
+        }
+      })
+    }
+  });
+  loadTweets();
+});
+
+
+
+
+////////////////////////////////////////////////////////////////////////
+// tweets rendering functions
+////////////////////////////////////////////////////////////////////////
+const escapeStr = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 
 const renderTweets = function(tweets) {
   tweets.forEach(function(tweet) {
     let $tweet = createTweetElement(tweet);
-    return $('#tweets-container').append($tweet);
+    return $('#tweets-container').prepend($tweet);
   })
 };
 
 
 const createTweetElement = function(tweet) {
+
+
   const user = tweet.user;
   const content = tweet.content;
-  const createdAt = '4 days ago';
+  const createdAt = timeago.format(tweet["created_at"]);
   let $tweet = $(`
   <article class="tweet">
   <header>
@@ -47,7 +78,7 @@ const createTweetElement = function(tweet) {
     </div>
   </header>
   <div class="tweet"> 
-    ${content.text} 
+    ${escapeStr(content.text)} 
   </div>
   <footer>
     <div class="dateCreated">
@@ -65,4 +96,12 @@ const createTweetElement = function(tweet) {
   return $tweet;
 }
 
-renderTweets(data);
+const loadTweets = function() {
+  $.ajax({
+    url: 'http://localhost:8080/tweets',
+    dataType: 'json',
+    success: function(data) {
+      renderTweets(data);
+    }
+  })
+};
---
+++ public/scripts/scroll.js
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////
+// stretch scroll functionality
+////////////////////////////////////////////////////////////////////////
+$(() => {
+  $('.bttButton').hide();
+
+  $(window).on('scroll', function() {
+    let scrollTop = $(window).scrollTop();
+    if (scrollTop > 0) {
+      $('nav').hide();
+      $('.bttButton').show();
+    } else {
+      $('nav').show();
+      $('.bttButton').hide();
+    }
+  });
+
+  $('.backToTop').on('click', function() {
+    window.scrollTo({
+      top: 0
+    })
+    $('.new-tweet').toggle(true);
+    $('#tweet-text').focus();
+  });
+});
